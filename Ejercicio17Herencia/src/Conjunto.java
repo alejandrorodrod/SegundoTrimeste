ccaf499bc5b91d6bf2d425c28b302f406df5e955
Alejandrodrod <alejandro.rodrod.6@iesjulianmarias.es>
2025-02-28 20:11:32 +0100

    Ejercicio 17

--- Ejercicio17Herencia/src/Conjunto.java
+++ Ejercicio17Herencia/src/Conjunto.java
@@ -1,76 +1,76 @@
 public class Conjunto extends Lista {
-        //Se crean los constructores
-        public Conjunto() {
-            super();
-        }
+    //Se crean los constructores
+    public Conjunto() {
+        super();
+    }
 
-        public int numeroElementos() {
-            return this.getElementosLista();
-        }
+    public int numeroElementos() {
+        return this.getElementosLista();
+    }
 
-        public boolean insertar(int elemento) {
-            boolean insertar = false;
-            if (this.getElementoEncontrar(elemento)==-1){
-                this.setElementoFinalLista(elemento);
+    public boolean insertar(int elemento) {
+        boolean insertar = false;
+        if (this.getElementoEncontrar(elemento) == -1) {
+            this.setElementoFinalLista(elemento);
+        }
+        return insertar;
+    }
+
+    public boolean insertar(Conjunto conjunto) {
+        boolean insertar = false;
+
+        for (int i = 0; i < conjunto.getElementosLista(); i++) {
+
+            if (this.getElementoEncontrar(conjunto.getElementoLista(i)) == -1) {
+                this.insertar(conjunto.getElementoLista(i));
             }
-            return insertar;
         }
 
-        public boolean insertar(Conjunto conjunto) {
-            boolean insertar = false;
+        return insertar;
+    }
 
-            for(int i = 0; i < conjunto.getElementosLista(); i++) {
+    public boolean eliminarElemento(int elemento) {
+        boolean eliminar = false;
+        int posicion = this.getElementoEncontrar(elemento);
 
-                if (this.getElementoEncontrar(conjunto.getElementoLista(i))==-1){
-                    this.insertar(conjunto.getElementoLista(i));
-                }
+        if (posicion != -1) {
+            eliminar = true;
+
+            for (; posicion < this.getElementosLista() - 1; posicion++) {
+                this.setElementoMitadLista(this.getElementoLista(posicion + 1), posicion);
             }
-
-            return insertar;
         }
 
-        public boolean eliminarElemento(int elemento) {
-            boolean eliminar = false;
-            int posicion = this.getElementoEncontrar(elemento);
-
-            if (posicion != -1) {
-                eliminar = true;
-
-                for (;posicion < this.getElementosLista() - 1; posicion++) {
-                    this.setElementoMitadLista(this.getElementoLista(posicion + 1) ,posicion);
-                }
-            }
-
-            if (eliminar) {
-                this.eliminarUltimoElemento();
-            }
-
-            return eliminar;
+        if (eliminar) {
+            this.eliminarUltimoElemento();
         }
 
-        public boolean eliminarConjunto(Conjunto conjunto) {
-            boolean eliminar = false;
+        return eliminar;
+    }
 
-            for (int i = 0; i < this.getElementosLista(); i++) {
-                this.eliminarElemento(conjunto.getElementoLista(i));
-            }
+    public boolean eliminarConjunto(Conjunto conjunto) {
+        boolean eliminar = false;
 
-            return eliminar;
+        for (int i = 0; i < this.getElementosLista(); i++) {
+            this.eliminarElemento(conjunto.getElementoLista(i));
         }
 
-        public boolean pertenece(int elemento) {
-            boolean pertenece = false;
-            int posicion = this.getElementoEncontrar(elemento);
+        return eliminar;
+    }
 
-            if (posicion != -1) {
-                pertenece = true;
-            }
+    public boolean pertenece(int elemento) {
+        boolean pertenece = false;
+        int posicion = this.getElementoEncontrar(elemento);
 
-            return pertenece;
+        if (posicion != -1) {
+            pertenece = true;
         }
 
-        public void mostrar() {
-            System.out.println("Conjunto:");
-            this.mostrarElementosLista();
-        }
+        return pertenece;
+    }
+
+    public void mostrar() {
+        System.out.println("Conjunto:");
+        this.mostrarElementosLista();
+    }
 }
