96331a867029d6dd8d7f070be63c2f72f954405b
Alejandrodrod <alejandro.rodrod.6@iesjulianmarias.es>
2025-03-05 19:40:36 +0100

    Ejercicio 18

--- Ejercicio17Herencia/src/Conjunto.java
+++ Ejercicio17Herencia/src/Conjunto.java
@@ -19,7 +19,7 @@ public class Conjunto extends Lista {
     public boolean insertar(Conjunto conjunto) {
         boolean insertar = false;
 
-        for (int i = 0; i < conjunto.getElementosLista(); i++) {
+        for (int i = 0; i < conjunto.numeroElementos(); i++) {
 
             if (this.getElementoEncontrar(conjunto.getElementoLista(i)) == -1) {
                 this.insertar(conjunto.getElementoLista(i));
@@ -36,7 +36,7 @@ public class Conjunto extends Lista {
         if (posicion != -1) {
             eliminar = true;
 
-            for (; posicion < this.getElementosLista() - 1; posicion++) {
+            for (; posicion < this.numeroElementos() - 1; posicion++) {
                 this.setElementoMitadLista(this.getElementoLista(posicion + 1), posicion);
             }
         }
@@ -51,7 +51,7 @@ public class Conjunto extends Lista {
     public boolean eliminarConjunto(Conjunto conjunto) {
         boolean eliminar = false;
 
-        for (int i = 0; i < this.getElementosLista(); i++) {
+        for (int i = 0; i < this.numeroElementos(); i++) {
             this.eliminarElemento(conjunto.getElementoLista(i));
         }
 
